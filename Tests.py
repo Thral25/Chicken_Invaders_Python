daed07951abd296a82f73c9fa5027043c91feca1
unknown <Thrall@.(none)>
2011-07-03 03:27:40 +0300

    Umm Tests arent working still have no idea why,this is wierd.

--- Tests.py
+++ Tests.py
@@ -1,4 +1,4 @@
-import unittest,pygame
+import unittest
 import Animation as anim
 meat=["eatme.png"]
 big_chick=["ship1.png","ship11.png"]
@@ -11,15 +11,24 @@ class InvadersTest(unittest.TestCase):
 		self.assertTrue(food.x,15)
 		self.assertTrue(food.y,20)
 		self.assertTrue(food.ttl,True)
-	 
-	def colision_when_mouse_pressed(self):
+		
+		
+	def update_duck_test(self):
 		duck_big=anim.Animation(50,60,big_chick)
 		duck_med=anim.Animation(100,200,mid_chick)
-		duck_small=anim.Animation(400,300,small_chick)
-		result=anim.Animation.collision(duck_big,duck_med,duck_small,55,65)
-		self.assertEqual(result,"big")
-		self.assertEqual(result,"med")
-		self.assertEqual(result,"small")
+		duck_small=anim.Animation(400,300,small_chick)		
+		duck_big.update(True)
+		self.assertTrue(duck_big.ttl,True)
+		self.assertNotTrue(duck_big.vx,0)
+		self.assertNotTrue(duck_big.vy,0)
+		self.assertNotTrue(len(duck_big.size),0)
+		
+	def update_food_test(self):	
+		food=anim.Animation(5,5,meat)
+		food.setfood(15,20)
+		food.update(False)
+		self.assertTrue(food.vx,0)
+		self.assertNotTrue(food.vy,0)
 		
 if __name__ == '__main__':
     unittest.main()	
